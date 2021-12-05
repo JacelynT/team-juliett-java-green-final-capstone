0e2bed81b4693d6802a8597bfe2b3436d3a03b42
Heather Carter <hcwallace00@gmail.com>
2021-12-05 14:24:28 -0500

    added first sql table

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,7 +1,11 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS child;
 DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS seq_child_id;
 DROP SEQUENCE IF EXISTS seq_user_id;
+
+
 
 CREATE SEQUENCE seq_user_id
   INCREMENT BY 1
@@ -9,6 +13,11 @@ CREATE SEQUENCE seq_user_id
   NO MINVALUE
   CACHE 1;
 
+CREATE SEQUENCE seq_child_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
 
 CREATE TABLE users (
 	user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
@@ -17,6 +26,18 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+CREATE TABLE child (
+	child_id int DEFAULT nextval('seq_child_id'::regclass) NOT NULL,
+	child_name varchar(50) NOT NULL,
+	user_id int NOT NULL,
+	icon_name varchar(50) NOT NULL,
+	CONSTRAINT PK_child PRIMARY KEY (child_id),
+	CONSTRAINT FK_child_users FOREIGN KEY (user_id) REFERENCES users(user_id)
+	
+);
+
+
+
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
