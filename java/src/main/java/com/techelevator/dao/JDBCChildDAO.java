25628ed4aaa7dd0323df5e507512c59c85eac54f
Zach Koors <koorsz@gmail.com>
2021-12-05 16:07:53 -0500

    completed listAllChildren method in JdbcChildDao, for dashboard page

--- java/src/main/java/com/techelevator/dao/JDBCChildDAO.java
+++ java/src/main/java/com/techelevator/dao/JDBCChildDAO.java
@@ -6,7 +6,9 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class JDBCChildDAO implements ChildDAO{
@@ -15,13 +17,28 @@ public class JDBCChildDAO implements ChildDAO{
 
     public List<Child> listAllChildren(int familyAccountId) {
         List<Child> childList = new ArrayList<>();
-        String sql = "SELECT * " +
-                "FROM child " +
-                "WHERE user.id = ?";
+        String sql = "SELECT child.child_id, child.child_name, SUM(child_book.minutes)\n" +
+                "FROM child\n" +
+                "JOIN child_book ON child.child_id = child_book.child_id\n" +
+                "WHERE user_id = ?\n" +
+                "GROUP BY child.child_id";
         SqlRowSet result = jdbcTemplate.queryForRowSet(sql, familyAccountId);
-        
+        while (result.next()){
+            Child child = mapRowToChild(result);
+            childList.add(child);
+        }
 
         return childList;
     }
 
+
+    private Child mapRowToChild(SqlRowSet result){
+        Child child = new Child();
+        child.setChildId(result.getInt("child_id"));
+        child.setName(result.getString("child_name"));
+        child.setMinutes(result.getInt("sum"));
+
+
+        return child;
+    }
 }
