5f581699021cb95f6ada951032bb0f1e98e38f93
Zach Koors <koorsz@gmail.com>
2021-12-05 15:31:02 -0500

    completed listAllChildren method in ChildController

--- java/src/main/java/com/techelevator/controller/ChildController.java
+++ java/src/main/java/com/techelevator/controller/ChildController.java
@@ -1,12 +1,15 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.ChildDAO;
+import com.techelevator.dao.UserDAO;
 import com.techelevator.model.Child;
+import com.techelevator.model.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -14,10 +17,13 @@ import java.util.List;
 public class ChildController {
 
     private ChildDAO childDAO;
+    private UserDAO userDAO;
+
 
     @RequestMapping(path = "/account", method  = RequestMethod.GET)
-    public List<Child> listAllChildren() {
-        return childDAO.listAllChildren();
+    public List<Child> listAllChildren(Principal familyAccount) {
+        int familyAccountId = userDAO.findIdByUsername(familyAccount.getName());
+        return childDAO.listAllChildren(familyAccountId);
     }
 
 }
