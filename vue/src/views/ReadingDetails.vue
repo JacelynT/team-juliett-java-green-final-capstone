8583a616c46ebffaf01b5b97a0811c143200c958
Zach Koors <koorsz@gmail.com>
2021-12-07 23:34:38 -0500

    added reading log form to ReadingDetails view

--- vue/src/views/ReadingDetails.vue
+++ vue/src/views/ReadingDetails.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
+    <reading-log-form />
       <book-list />
   </div>
 </template>
 
 <script>
 import BookList from '../components/BookList.vue';
+import ReadingLogForm from '../components/ReadingLogForm.vue';
 export default {
-  components: { BookList },
+  components: { 
+    BookList,
+    ReadingLogForm 
+    },
     name: "reading-details"
 
 }
