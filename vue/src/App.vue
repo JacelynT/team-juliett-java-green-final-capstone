4f690e5aca318d0016ab789a1de14592e26deb21
Zach Koors <koorsz@gmail.com>
2021-12-08 11:14:07 -0500

    created book cover component

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -7,22 +7,29 @@
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue'
-import AppFooter from './components/AppFooter.vue'
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
 
 export default {
-
-
-components: {
-  AppHeader,
-  AppFooter
-}
-
-}
+  data() {
+    return {
+      book: {
+        isbn: "9780007158447",
+        minutes: 45,
+        childId: 1,
+        date: "11-05-2021",
+      },
+    };
+  },
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
 </script>
 
 <style>
-html{
+html {
   font-family: sans-serif;
 }
 </style>
