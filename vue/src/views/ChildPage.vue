f8cfedab994c2554d748bf30d0d16f74d904e40f
Zach Koors <koorsz@gmail.com>
2021-12-10 10:11:18 -0500

    added bookCards to child page

--- vue/src/views/ChildPage.vue
+++ vue/src/views/ChildPage.vue
@@ -2,13 +2,7 @@
   <div>
     <h2 class="display-5">Books I'm Reading</h2>
     <div id="current-book-container">
-      <div class="current-book" v-for="book in currentBooks" v-bind:key="book.id">
-        <img v-bind:src="
-          'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'
-        " alt="">
-        <h3>{{book.title}}</h3>
-        <h4>{{book.minutes}} min</h4>
-      </div>
+      <book-card class="book-card" v-for="book in currentBooks" v-bind:book="book" v-bind:key="book.id"/>
     </div>
     <hr>
     <h2 class="display-5">Family Library</h2>
@@ -20,18 +14,22 @@
 </template>
 
 <script>
+import BookCard from '../components/BookCard.vue';
 import SmallBook from '../components/SmallBook.vue';
 // import ReadingTrackerService from "../services/ReadingTrackerService.js"
 
 export default {
   data() {
     return {
-      currentBooks: [],
+      currentBooks: [
+        {"isbn":"9780375810886","title":"Green Eggs and Ham","author":"Dr. Seuss","minutes":0,"active":true},{"isbn":"9780439554923","title":"Harry Potter and the Prisoner of Azkaban","author":"J.K. Rowling","minutes":0,"active":true},{"isbn":"9781728234939","title":"How to Catch a Unicorn","author":"Adam Wallace","minutes":0,"active":true},
+      ],
       library: [],
     };
   },
   components: {
-    SmallBook
+    SmallBook,
+    BookCard
   },
   name: "child-page",
   created(){
@@ -41,6 +39,9 @@ export default {
 </script>
 
 <style scoped>
+book-card{
+  padding: 20px;
+}
 img {
   border-radius: 1rem;
 }
@@ -48,8 +49,9 @@ img {
   display: flex;
   flex-wrap: wrap;
   flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  margin-bottom: 75px;
 }
 #library-container {
   display: flex;
