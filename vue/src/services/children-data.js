36ee750f9f384b82e1470f828ec22eaaa0c8fd93
Heather Carter <hcwallace00@gmail.com>
2021-12-15 14:25:56 -0500

    changed grid area on account for new chart

--- vue/src/services/children-data.js
+++ vue/src/services/children-data.js
@@ -1,18 +1,18 @@
 export const childrenChartData = {
     type: "line",
     data: {
-      labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Neptune"],
+      labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
       datasets: [
         {
-          label: "Number of Moons",
-          data: [0, 0, 1, 2, 79, 82, 27, 14],
+          label: "Gordon",
+          data: [60, 30, 20, 20, 40, 20, 70],
           backgroundColor: "rgba(54,73,93,.5)",
           borderColor: "#36495d",
           borderWidth: 3
         },
         {
-          label: "Planetary Mass (relative to the Sun x 10^-6)",
-          data: [0.166, 2.081, 3.003, 0.323, 954.792, 285.886, 43.662, 51.514],
+          label: "Autumn",
+          data: [20, 20, 20, 20, 20, 20, 40],
           backgroundColor: "rgba(71, 183,132,.5)",
           borderColor: "#47b784",
           borderWidth: 3
